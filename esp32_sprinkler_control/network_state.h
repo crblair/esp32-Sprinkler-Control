e9da66127bf84a809a65b181fe8c738eb802d283
Chris <chrisblair@blairpage.com>
2025-05-04 16:58:29 -0400

    Add NetworkState class with ssid member (no logic change, prep for safe encapsulation)

---
+++ esp32_sprinkler_control/network_state.h
@@ -0,0 +1,12 @@
+// network_state.h
+// Safe introduction: empty class for future network encapsulation
+#ifndef NETWORK_STATE_H
+#define NETWORK_STATE_H
+
+class NetworkState {
+public:
+    String ssid;
+    NetworkState() : ssid("") {}
+};
+
+#endif // NETWORK_STATE_H
